7367cc497be780ea34a3a04f52d865a32cd532c8
konradm <konrad.marszalek@gmail.com>
2014-09-14 18:57:39 +0200

    Add better error handling for login errors

--- features/pages/login_page.rb
+++ features/pages/login_page.rb
@@ -6,11 +6,15 @@ class LoginPage
   text_field(:email, :id => 'user_email')
   text_field(:password, :id => 'user_password')
   button(:login, :xpath => "//button[i[contains(@class, 'icon-lock')]]")
+  div(:error, :class =>'alert-error')
 
   def login_with(email, password)
     self.email = email
     self.password = password
     login
+    if error?
+      raise "Error occurred during logging in: '#{error}'. You credentials were #{email} and #{password}."
+    end
   end
 
 end
